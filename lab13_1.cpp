365f71f1020ce0fd5fb70643df230dae6706ce95
Thanadett <tanapremin@gmail.com>
2024-01-02 15:23:32 +0700

    lab13_1.cpp completed

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -2,13 +2,14 @@
 #include <cmath>
 using namespace std;
 
-void stat(const double[],int,double[]);
+void stat(const double[], int, double[]);
 
-int main(){
-    double A[] = {1.2,3.5,6.9,7.8,12.5,0.5};
-    int N = sizeof(A)/sizeof(A[0]);
+int main()
+{
+    double A[] = {1.2, 3.5, 6.9, 7.8, 12.5, 0.5};
+    int N = sizeof(A) / sizeof(A[0]);
     double B[6];
-    stat(A,N,B);
+    stat(A, N, B);
     cout << "Arithmetic Mean = " << B[0];
     cout << "\nStandard Deviation = " << B[1];
     cout << "\nGeometric Mean = " << B[2];
@@ -16,4 +17,52 @@ int main(){
     cout << "\nMax = " << B[4];
     cout << "\nMin = " << B[5];
     return 0;
+}
+
+void stat(const double A[], int size, double B[])
+{
+    double sum = 0;
+    for (int i = 0; i < size; i++)
+    {
+        sum += A[i];
+    }
+    B[0] = sum / size;
+
+    double mean = B[0];
+    double std = 0;
+    for (int i = 0; i < size; i++)
+    {
+        std += pow(A[i] - mean, 2);
+    }
+    B[1] = sqrt(std / size);
+
+    double gome = 1.0;
+    for (int i = 0; i < size; i++)
+    {
+        gome *= A[i];
+    }
+    B[2] = pow(gome, 1.0 / size);
+
+    double harmo = 0;
+    for (int i = 0; i < size; i++)
+    {
+        harmo += 1 / A[i];
+    }
+    B[3] = size / harmo;
+
+    double max = A[0];
+    double min = A[0];
+    for (int i = 1; i < size; i++)
+    {
+        if (A[i] > max)
+        {
+            max = A[i];
+        }
+        if (A[i] < min)
+        {
+            min = A[i];
+        }
+    }
+    B[4] = max;
+    B[5] = min;
 }
